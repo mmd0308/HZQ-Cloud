c1e4a6ce6592d758bb703634cb8d430a51993ddc
hengzhaoqing <hengzhaoqing@163.com>
2019-08-13 22:21:53 +0800

    新增统一的ip拦截接口

--- hzq-common/hzq-common-core/src/main/java/com/hzqing/common/core/service/request/CheckIdRequest.java
+++ hzq-common/hzq-common-core/src/main/java/com/hzqing/common/core/service/request/CheckIdRequest.java
@@ -1,8 +1,32 @@
 package com.hzqing.common.core.service.request;
 
+import com.hzqing.common.core.service.constants.CommonRetCodeConstants;
+import com.hzqing.common.core.service.exception.ParamsValidateException;
+import lombok.Data;
+import lombok.ToString;
+
 /**
+ * id 主键校验
  * @author hzqing
  * @date 2019-08-13 21:11
  */
-public class CheckIdRequest {
+@Data
+@ToString(callSuper = true)
+public class CheckIdRequest extends AbstractRequest {
+
+    private static final long serialVersionUID = 4085265036094778483L;
+    /**
+     * 主键
+     */
+    private Integer id;
+
+    @Override
+    public void checkParams() {
+        if (id == null || id == 0){
+            throw new ParamsValidateException(
+                    CommonRetCodeConstants.INVALID_PARAMETER.getCode(),
+                    "主键ID,不能为空"
+            );
+        }
+    }
 }
